37625f815136612ce2bbfae782ec3a080651b1a3
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-01-03 19:47:32 -0300

    Desenvolvendo a logica das funções que geram letras, numeros e caracteres aleatorios

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 // seleção de Elementos
-const generatePasswordButton = document.querySelector('#generate-password');
-const generatePasswordElement = document.querySelector('#generated-password');
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatePasswordElement = document.querySelector("#generated-password");
 
 // Funções
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
 
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {2
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+    const symbols = "()!@#$%¨&*{}[]<>?-+";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+console.log(getLetterLowerCase());
+console.log(getLetterUpperCase());
+console.log(getNumber());
+console.log(getSymbol());
 
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
-    console.log("funfou")
-})
+    console.log("funfou");
+});

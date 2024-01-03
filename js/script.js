6e774a1ab4260709dacfa202c8462d430661ea92
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-01-03 18:20:49 -0300

    iniciando a seleção dos elemento no js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// seleção de Elementos
+const generatePasswordButton = document.querySelector('#generate-password');
+const generatePasswordElement = document.querySelector('#generated-password');
+
+// Funções
+
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+    console.log("funfou")
+})

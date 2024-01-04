a2b527b2d5186b648bc1750dcbc6519ccbdcf4aa
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-01-04 17:41:57 -0300

    desenvolvimento de um read-me.md, resolução de bugs no codigo.js e realização de alguns ajustes no css

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatePasswordElement = document.querySelector("#generated-password");
 const openCloseGeneratorButton = document.querySelector(
-  "#open-generate-password"
+    "#open-generate-password"
 );
 const generatePasswordContainer = document.querySelector("#generate-options");
 const lengthInput = document.querySelector("#length");
@@ -13,80 +13,80 @@ const copyPasswordButton = document.querySelector("#copy-password");
 
 // Funções
 const getLetterLowerCase = () => {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
 
 const getLetterUpperCase = () => {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 
 const getNumber = () => {
-  return Math.floor(Math.random() * 10).toString();
+    return Math.floor(Math.random() * 10).toString();
 };
 
 const getSymbol = () => {
-  const symbols = "()!@#$%¨&*{}[]<>?-+";
-  return symbols[Math.floor(Math.random() * symbols.length)];
+    const symbols = "()!@#$%¨&*{}[]<>?-+";
+    return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
 const generatePassword = (
-  getLetterLowerCase,
-  getLetterUpperCase,
-  getNumber,
-  getSymbol
-) => {
-  let password = "";
-
-  // Segunda Versão
-
-  const passwordLength = +lengthInput.value;
-
-  const generators = [];
-
-  if (lettersInput.checked) {
-    generators.push(getLetterLowerCase, getLetterUpperCase);
-  };
-
-  if (numbersInput.checked) {
-    generators.push(getNumber);
-  };
-
-  if (symbolsInput.checked) {
-    generators.push(getSymbol);
-  };
-
-  if (generators.length === 0) {
-    window.alert("Selecione alguma opção para gerar a senha");
-    return;
-  };
-
-  for (let i = 0; i < passwordLength; i = i + generators.length) {
-    generators.forEach(() => {
-      const randomValue =
-        generators[Math.floor(Math.random() * generators.length)]();
-
-      password += randomValue;
-    });
-  };
-
-  password = password.slice(0, passwordLength);
-
-  generatePasswordElement.style.display = "block";
-  generatePasswordElement.querySelector("h4").innerText = password;
-};
-
-// Eventos
-generatePasswordButton.addEventListener("click", () => {
-  generatePassword(
     getLetterLowerCase,
     getLetterUpperCase,
     getNumber,
     getSymbol
-  );
+) => {
+    let password = "";
+
+    // Segunda Versão
+
+    const passwordLength = +lengthInput.value;
+
+    const generators = [];
+
+    if (lettersInput.checked) {
+        generators.push(getLetterLowerCase, getLetterUpperCase);
+    };
+
+    if (numbersInput.checked) {
+        generators.push(getNumber);
+    };
+
+    if (symbolsInput.checked) {
+        generators.push(getSymbol);
+    };
+
+    if (generators.length === 0) {
+        window.alert("Selecione alguma opção para gerar a senha");
+        return;
+    };
+
+    for (let i = 0; i < passwordLength; i = i + generators.length) {
+        generators.forEach(() => {
+            const randomValue =
+                generators[Math.floor(Math.random() * generators.length)]();
+
+            password += randomValue;
+        });
+    };
+
+    password = password.slice(0, passwordLength);
+
+    generatePasswordElement.style.display = "block";
+    generatePasswordElement.querySelector("h4").innerText = password;
+};
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+    generatePassword(
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    );
 });
 
 openCloseGeneratorButton.addEventListener("click", () => {
-  generatePasswordContainer.classList.toggle("hide");
+    generatePasswordContainer.classList.toggle("hide");
 });
 
 copyPasswordButton.addEventListener("click", (e) => {
